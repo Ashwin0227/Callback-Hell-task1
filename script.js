6732a203de0ef8d82882deaa460fd0db8c7b2616
Ashwin kumar .S <117738392+Ashwin0227@users.noreply.github.com>
2023-03-20 13:51:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+// Countdown starts
+setTimeout(()=>{
+    document.getElementById('number').innerHTML = 10;
+    setTimeout(()=>{
+        document.getElementById('number').innerHTML = 9;
+        setTimeout(()=>{
+            document.getElementById('number').innerHTML = 8;
+            setTimeout(()=>{
+                document.getElementById('number').innerHTML = 7;
+                setTimeout(()=>{
+                    document.getElementById('number').innerHTML = 6;
+                    setTimeout(()=>{
+                        document.getElementById('number').innerHTML = 5;
+                        setTimeout(()=>{
+                            document.getElementById('number').innerHTML=4;
+                            setTimeout(()=>{
+                                document.getElementById('number').innerHTML=3;
+                                setTimeout(()=>{
+                                    document.getElementById('number').innerHTML = 2;
+                                    setTimeout(()=>{
+                                        document.getElementById('number').innerHTML = 1;
+                                        setTimeout(()=>{
+                                            document.getElementById('number').innerHTML = 'Happy Independence Day';
+                                        },1000)
+                                    },1000)
+                                },1000)
+                            },1000)
+                        },1000)
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},0)
